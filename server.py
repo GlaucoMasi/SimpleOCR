6c63ab27e1523d391feddaf98438512b5f395681
Glauco <glaucomasi@gmail.com>
2025-05-11 00:52:58 +0200

    html, js e server

---
+++ server.py
@@ -0,0 +1,33 @@
+import json
+
+def do_Post(s):
+  response_code = 200
+  response = ""
+  var_len = int(s.headers.get('Content-Length'))
+  content = s.rfile.read(var_len)
+  payload = json.loads(content)
+
+  if payload.get('train'):
+    nn.train(payload['trainArray'])
+    nn.save()
+  elif payload.get('predict'):
+    try:
+      response = {
+        "type":"test",
+        "result":nn.predict(str(payload['image']))
+      }
+    except:
+      response_code = 500
+  else:
+    response = 400
+  
+  s.send_response(response_code)
+  s.send_header("Content-type", "application/json")
+  s.send_header("Access-Control-Allow-Origin", "*")
+  s.end_headers()
+  
+  if response:
+    s.wfile.write(json.dumps(response))
+  
+  return
+    
